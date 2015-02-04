539535f82ae8aa1576b3c1fda683ff0c63cd41e2
TJ Horner <me@tjhorner.com>
2015-02-03 21:12:36 -0800

    yo

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AutoShort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.hideButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "short is running.";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "short";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "short";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "last url shortened:";
+            // 
+            // hideButton
+            // 
+            this.hideButton.Location = new System.Drawing.Point(313, 4);
+            this.hideButton.Name = "hideButton";
+            this.hideButton.Size = new System.Drawing.Size(39, 23);
+            this.hideButton.TabIndex = 2;
+            this.hideButton.Text = "hide";
+            this.hideButton.UseVisualStyleBackColor = true;
+            this.hideButton.Click += new System.EventHandler(this.hideButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 57);
+            this.Controls.Add(this.hideButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "short";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button hideButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+
+namespace AutoShort
+{
+    public partial class Form1 : Form
+    {
+        IntPtr nextClipboardViewer;
+
+        [DllImport("User32.dll")]
+        protected static extern int
+            SetClipboardViewer(int hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool
+               ChangeClipboardChain(IntPtr hWndRemove,
+                                    IntPtr hWndNewNext);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg,
+                                             IntPtr wParam,
+                                             IntPtr lParam);
+
+        protected override void
+          WndProc(ref System.Windows.Forms.Message m)
+        {
+            const int WM_DRAWCLIPBOARD = 0x308;
+            const int WM_CHANGECBCHAIN = 0x030D;
+
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    if (Clipboard.ContainsText() && Regex.IsMatch(Clipboard.GetText(), @"((https?:)?//)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,63}(:[\d]+)?(/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?"))
+                    {
+                        if (Clipboard.GetText().StartsWith("http://short.tjhorner.com") || Clipboard.GetText().StartsWith("http://puu.sh"))
+                        {
+                            break;
+                        }
+                        using (var wb = new WebClient())
+                        {
+                            var data = new NameValueCollection();
+                            data["url"] = Clipboard.GetText();
+
+                            label2.Text = "last url shortened: " + Clipboard.GetText();
+
+                            byte[] response = wb.UploadValues("http://short.tjhorner.com/create.json", "POST", data);
+
+                            string strRes = Encoding.UTF8.GetString(response);
+
+                            JObject res = JObject.Parse(strRes);
+
+                            if ((bool)res.GetValue("success"))
+                            {
+                                Clipboard.SetText("http://short.tjhorner.com/" + res.GetValue("shortId"));
+                            }
+                        }
+                    }
+                    SendMessage(nextClipboardViewer, m.Msg, m.WParam,
+                                m.LParam);
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam == nextClipboardViewer)
+                        nextClipboardViewer = m.LParam;
+                    else
+                        SendMessage(nextClipboardViewer, m.Msg, m.WParam,
+                                    m.LParam);
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            nextClipboardViewer = (IntPtr)SetClipboardViewer((int) this.Handle);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void hideButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
